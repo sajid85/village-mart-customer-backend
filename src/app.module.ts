e7cd409b90aa8632bdc1f15d3bb35cc971f4d2f0
sajid85 <sajidalam4531@gmail.com>
2025-06-09 20:39:05 +0600

    render database code connected

--- src/app.module.ts
+++ src/app.module.ts
@@ -24,6 +24,8 @@ import { OrdersModule } from './orders/orders.module';
         database: configService.get('DB_DATABASE', 'villagemart'),
         entities: [__dirname + '/**/*.entity{.ts,.js}'],
         synchronize: configService.get('DB_SYNC', true),
+        // --- ADD SSL SUPPORT ---
+        ssl: { rejectUnauthorized: false }, // <-- THIS LINE is required for Render!
       }),
       inject: [ConfigService],
     }),
